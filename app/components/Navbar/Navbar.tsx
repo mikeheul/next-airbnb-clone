0bc16c50f61eac735d111c776fbd6440ee3edbfe
Micka <mickael.murmann@gmail.com>
2024-03-19 14:51:48 +0100

    Create Navbar and Container components

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Container from "../Container";
+
+const Navbar = () => {
+    return ( 
+        <nav className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    
+                </Container>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Nunito } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 const nunito = Nunito({ subsets: ["latin"],})
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
