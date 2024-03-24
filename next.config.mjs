c3f6301f098fc1c3f0d284be3978e8029fc6807f
Micka <mickael.murmann@gmail.com>
2024-03-24 12:09:40 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "avatars.githubusercontent.com",
-            "lh3.googleusercontent.com"
+        remotePatterns: [
+            {
+                protocol:'https',
+                hostname: 'avatars.githubusercontent.com'
+            },
+            {
+                protocol:'https',
+                hostname: 'lh3.googleusercontent.com'
+            },
+            // "avatars.githubusercontent.com",
+            // "lh3.googleusercontent.com"
         ]
     }
 };
