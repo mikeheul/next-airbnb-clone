9e13d56fc3370cb2322e3800888a1779a9ece371
Micka <mickael.murmann@gmail.com>
2024-03-28 08:27:58 +0100

    Listing detail view (calendar, map)

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -7,14 +7,14 @@ import { Range } from "react-date-range";
 import { useRouter } from "next/navigation";
 import { differenceInDays, eachDayOfInterval } from 'date-fns';
 
-import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 
 import Container from "@/app/components/Container";
-import { categories } from "@/app/components/navbar/Categories";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
 import ListingReservation from "@/app/components/listings/ListingReservation";
+import useLoginModal from "@/app/components/hooks/useLoginModal";
+import { categories } from "@/app/components/Navbar/Categories";
 
 const initialDateRange = {
     startDate: new Date(),
